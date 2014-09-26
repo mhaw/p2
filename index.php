9bfab358138b3bc6c85df981aaf1d57b2f0fba98
Mike Haw <mikehaw@gmail.com>
2014-09-26 14:54:01 -0400

    first attempt at password building logic

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
+ini_set('display_errors', 1); # Display errors on page (instead of a log file)
+?>
+
+<?php require('logic.php');?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -18,18 +25,21 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    
   </head>
   <body>
   	<div class="container">
     	
-    	<form class="form-signin" role="form">
+    	<form class="form-signin" role="form" method="POST" action="">
     		<h2 class="form-signin-heading">xkcd Password Generator</h2>
+
     		Number of Words: <input type="text" name="number_words"><br>
 			<input type="checkbox" class="form-control" name="include_number"> Include a number?<br>
+
 			<input type="checkbox" class="form-control" name="include_symbol"> Include a symbol?<br>
 			<br>
 		
-			<button class="btn btn-lg btn-primary btn-block" type="submit">Submit</button>
+			<p><button type="submit" class="btn btn-primary">Submit</button></p>
 		</form>
   	</div>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
--- logic.php
+++ logic.php
@@ -1,8 +1,40 @@
+<?php
+error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
+ini_set('display_errors', 1); # Display errors on page (instead of a log file)
+?>
+
 <?php 
 
-	$final_pass = passgen('');
+	
+	$numwords = $_POST["number_words"];
+	$words = file('data/words.txt');
 	
 
+	if(isset($_POST["include_symbol"])) {
+	$available_symbols = array('!', '@', '#', '$', '%', '&');
+		shuffle($available_symbols);
+		$sym = $available_symbols[0];
+	}
+
+	if(isset($_POST["include_number"])) {
+		$num = rand(0,9);
+	}
+
+	for ($i=0; $i < $numwords-1 ; $i++) { 
+	 $rand_num = rand(0, count($words)-1);
+	 $pass_words[$i] = rtrim($words[$rand_num]);
+	}
+
+	$seperated_words = implode('-', $pass_words);
+
+	// for testing
+	echo $numwords;
+	echo '<br>';
+	echo $sym;
+	echo '<br>';
+	echo $num;
+	echo '<br>';
+	echo $seperated_words;
 
 
  ?> 
