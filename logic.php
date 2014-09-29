49e722a98b65ce31c685d1cf5f954914c4ba39f9
Mike Haw <mikehaw@gmail.com>
2014-09-29 12:26:00 -0400

    updated logic to handle null numwords entry, and logic to handle numwords > 20

--- logic.php
+++ logic.php
@@ -4,12 +4,19 @@ ini_set('display_errors', 1); # Display errors on page (instead of a log file)
 ?>
 
 <?php 
-
+	$numwords = 4;
 	
 	if(isset($_POST["number_words"])) {
-		$numwords = $_POST["number_words"];
+		$numwords = $_POST["number_words"] + 0; 
+		//add zero so value will not be undefined if no value entered by user
+
 	}
-	else $numwords = 4;
+
+	//if numwords was not entered default to 4
+	if($numwords == 0) {
+		$numwords = 4;
+	}
+
 
 	$words = file('data/words.txt');
 
@@ -28,7 +35,11 @@ ini_set('display_errors', 1); # Display errors on page (instead of a log file)
 	}
 	else $num = '';
 
-	for ($i=0; $i < $numwords ; $i++) { 
+	if($numwords > 20) {
+		echo "Please enter a number between 1 and 20";
+	} else {
+
+	for ($i=0; $i < $numwords; $i++) { 
 	 $rand_num = rand(0, count($words)-1);
 	 $pass_words[$i] = rtrim($words[$rand_num]);
 	}
@@ -38,10 +49,12 @@ ini_set('display_errors', 1); # Display errors on page (instead of a log file)
 	if(isset($_POST["all_upper"])) {
 		$seperated_words = strtoupper($seperated_words);
 	}
-	
 
-	// for testing;
 	echo  $seperated_words.$num.$sym;
+	
+	}
+	
+	
 
 
  ?> 
