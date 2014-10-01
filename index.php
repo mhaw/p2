3977071d5ed037bada22ee78e52efbb0301ace06
Mike Haw <mikehaw@gmail.com>
2014-10-01 01:09:10 -0400

    updated layout, added comments to logic

--- index.php
+++ index.php
@@ -2,109 +2,79 @@
 error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
 ini_set('display_errors', 1); # Display errors on page (instead of a log file)
 ?>
-
-
-
 <!DOCTYPE html>
+
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>xkcd Password Generator</title>
 
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    
-    <!-- Custom CSS -->
-    <link href="css/style.css" rel="stylesheet">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <title>xkcd Password Generator</title><!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"><!-- Custom CSS -->
+    <link href="css/style.css" rel="stylesheet" type="text/css"><!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    
-  </head>
-  <body>
-  	<div class="container">
+</head>
 
+<body>
+    <div class="container">
         <div class="row">
-          <div class="col-sm-3">
-   
-  </div>
+            <div class="col-sm-3"></div>
 
-      <div class="col-lg-6 text-center">
-    	
-      
+            <div class="col-lg-6 text-center">
+                <form role="form" method="post" action="logic.php">
+                    <h1>xkcd password generator</h1>
 
-    	<form crole="form" method="POST" action="">
-    		<h1>xkcd password generator</h1>
-          <div class="intro">
-            <p>A simple password generator based on <a href="http://xkcd.com/936/">xKcd's 
-            classic password comic</a>. This generator will construct a password of 
-            random english words. Options include the ability to add a special symbol, 
-            number (1-9), make the password all uppercase, and select the word 
-            separator type. <a href="https://howsecureismypassword.net/">
-            Test your password strength here. </a></p>
-          </div>
-          <br>
-          <p class="bg-primary"><?php require('logic.php');?></p>
-          <br>
+                    <div class="intro">
+                        <p>A simple password generator based on <a href="http://xkcd.com/936/">xKcd's classic password comic</a>. This generator will construct a password of random English words. Options include adding a special symbol or numerical digit, making the password all uppercase, and selecting the word separator punctuation. <a href="https://howsecureismypassword.net/">Test your password strength here.</a></p>
+                    </div><br>
 
+                    <p class="bg-primary"><?php require('logic.php');?></p><br>
+                    <input type="number" class="form-control" placeholder="number of words to include in your password" name="number_words"><br>
 
-    		<input type="number" class="form-control" placeholder="number of words to include in your password" name="number_words"><br>
+                    <div class="checkbox">
+                        <label><input type="checkbox" name="include_number"> include a number?</label>
+                    </div>
 
-        <div class="checkbox">
-          <label>
-            <input type="checkbox" name="include_number">
-            include a number?
-          </label>
+                    <div class="checkbox">
+                        <label><input type="checkbox" name="include_symbol"> include a symbol?</label>
+                    </div>
+
+                    <div class="checkbox">
+                        <label><input type="checkbox" name="all_upper"> all uppercase?</label>
+
+                        <div class="select">
+                            <label>separator type?</label><br>
+                            <select class="form-control" name="septype">
+                                <option value="space">
+                                    space
+                                </option>
+
+                                <option value="underscore">
+                                    _ underscore
+                                </option>
+
+                                <option value="hypen">
+                                    - hypen
+                                </option>
+                            </select>
+                        </div>
+                    </div><button type="submit" class="btn btn-primary">Submit</button>
+                </form><br>
+                <br>
+                <a href="http://xkcd.com/936/"><img src="http://imgs.xkcd.com/comics/password_strength.png" alt="xkcd comic"></a>
+            </div>
+
+            <div class="col-sm-3"></div>
         </div>
-        <div class="checkbox">
-          <label>
-            <input type="checkbox" name="include_symbol">
-            include a symbol?
-          </label>
-        </div>
-         <div class="checkbox">
-          <label>
-            <input type="checkbox" name="all_upper">
-            all uppercase?
-          </label>
-        <div class="select">
-          <label>
-            seperator type?
-          </label> <br>
-          <select class="form-control" name="septype">
-            <option value="space">  space</option>
-            <option value="underscore">_ underscore</option>
-            <option value="hypen">- hypen</option>
-          </select>
-          
-        
-        </div>
-
-        </div>
-			<button type="submit" class="btn btn-primary">Submit</button>
-		</form>
-    <br>
-    <br>
-    <a href="http://xkcd.com/936/"><img src="http://imgs.xkcd.com/comics/password_strength.png"></a>
-
-
-  </div>
-  <div class="col-sm-3">
-
-
-   
-  </div>
-  </div>
-  	</div>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-  </body>
+    </div><!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js" type="text/javascript">
+</script><!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js" type="text/javascript">
+</script>
+</body>
 </html>
--- logic.php
+++ logic.php
@@ -3,63 +3,65 @@ error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
 ini_set('display_errors', 1); # Display errors on page (instead of a log file)
 ?>
 
-<?php 
+<?php
+$numwords = 4;
+$separator = ' ';
+
+if(isset($_POST["number_words"])) {
+	$numwords = $_POST["number_words"] + 0;
+	//add zero so value will not be undefined if no value entered by user
+
+}
+
+//if numwords was not entered default to 4
+if($numwords == 0) {
 	$numwords = 4;
-	$separator = ' ';
-	
-	if(isset($_POST["number_words"])) {
-		$numwords = $_POST["number_words"] + 0; 
-		//add zero so value will not be undefined if no value entered by user
+}
 
+//generate word array
+$words = file('data/words.txt');
+
+$sym = '';
+$num = '';
+
+//logic to customize separator type
+if(isset($_POST["septype"])) {
+	$septype = $_POST["septype"];
+
+	if($septype == 'hypen'){
+		$separator = '-';
 	}
-
-	//if numwords was not entered default to 4
-	if($numwords == 0) {
-		$numwords = 4;
+	if($septype == 'underscore') {
+		$separator = '_';
 	}
-
-	//generate word array
-	$words = file('data/words.txt');
-
-	$sym = '';
-	$num = '';
-
-	//logic to customize separator type
-	if(isset($_POST["septype"])) {
-		$septype = $_POST["septype"];
-
-		if($septype == 'hypen'){
-			$separator = '-';
-		}
-		if($septype == 'underscore') {
-			$separator = '_';
-		}
-		if($septype == 'space') { 
-			$separator = ' ';
-		}
+	if($septype == 'space') {
+		$separator = ' ';
 	}
+}
 
-	//logic to pick and insert random symbol
-	if(isset($_POST["include_symbol"])) {
+//logic to pick and insert random symbol
+if(isset($_POST["include_symbol"])) {
 	$available_symbols = array('!', '@', '#', '$', '%', '&');
-		shuffle($available_symbols);
-		$sym = $available_symbols[0];
-	}
-	else $sym = '';
+	shuffle($available_symbols);
+	$sym = $available_symbols[0];
+}
+else $sym = '';
 
-	//insert a number from 
-	if(isset($_POST["include_number"])) {
-		$num = rand(0,9);
-	}
-	else $num = '';
+//insert a number from
+if(isset($_POST["include_number"])) {
+	$num = rand(0,9);
+}
+else $num = '';
 
-	if($numwords > 20) {
-		echo "Please enter a number between 1 and 20";
-	} else {
-
-	for ($i=0; $i < $numwords; $i++) { 
-	 $rand_num = rand(0, count($words)-1);
-	 $pass_words[$i] = rtrim($words[$rand_num]);
+//check for number entry between 1 and 20
+if($numwords < 1 || $numwords > 20) {
+	echo "Please enter a number between 1 and 20";
+} else {
+	//if number is valid proceed to pull random words, insert symbols and number 
+	//if requested and concatenate final password
+	for ($i=0; $i < $numwords; $i++) {
+		$rand_num = rand(0, count($words)-1);
+		$pass_words[$i] = rtrim($words[$rand_num]);
 	}
 
 	$seperated_words = implode($separator, $pass_words);
@@ -69,10 +71,7 @@ ini_set('display_errors', 1); # Display errors on page (instead of a log file)
 	}
 
 	echo  $seperated_words.$num.$sym;
-	
-	}
-	
-	
 
+}
 
- ?> 
+?> 
